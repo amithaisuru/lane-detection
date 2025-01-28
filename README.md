f46c5532a294ca14cada0401df509ad2b03005a7
amithaisuru <rkamithaisuru@gmail.com>
2025-01-28 22:35:25 +0530

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # lane-detection
 detect lanes of a road using dash a cam video. Used Sobel edge detection and Hough transformation
+
+## Model based lane detection
+
+Find two line models of lane markers, one from the left size and the other from the right side of the current vehicle.
+
+![image](https://user-images.githubusercontent.com/7046889/198094792-7992c1e4-3729-4484-9b03-beb5a606380b.png)
+
+To simplify the detection problem, we assume the following constraints
+   1. Only two lane markers will be detected (line1, line2)
+   2. Only interest area below of certain ```y``` value will be processed. 
+   3. Line1 has negative slope (-0.5~-1.5), while line2 has positive slope (0.5~1.5).
+
+![image](https://user-images.githubusercontent.com/7046889/198095755-b81086c1-c90c-4886-adcf-5b4880b38654.png)
+
+## Detection steps
+
+  1. Convert an input image to a grayscale image
+  2. Apply Sobel edge detection algorithm 
+
+![image](https://user-images.githubusercontent.com/7046889/198096068-dab3c791-bfbb-411a-b7a5-4781c2003d26.png)
+
+  3. From the vehicle’s current center (x position), search the first edge pixel in each left and right direction (Do it in all scanlines). The edge angle of the first edge pixel from the vehicle center is 0 degree in the right side, and 180 degree in the left side. 
+
+![image](https://user-images.githubusercontent.com/7046889/198096585-dc5da48f-783d-4034-a773-09300083cd01.png)
+![image](https://user-images.githubusercontent.com/7046889/198097149-60a57e94-0743-4019-9ccc-bb678cd82dac.png)
+
+  4. Using the edge pixels, apply the Hough transform to determine the two line models.
+  
+![image](https://user-images.githubusercontent.com/7046889/198097248-cef4a660-dbdd-444d-80a3-a33b26c52082.png)
+
+  5. Draw two lines over the original road image. At the intersection of the lines, draw a circle which represents the vanishing point.  Draw the two lines from the image border to the vanishing point.  
+  
+![image](https://user-images.githubusercontent.com/7046889/198098128-2db5a1b4-491b-4231-a814-1aef0157fbb1.png)
+
+  6. Apply the same lane detection algorithm to other video frames. 
+  
+  
+## Submissions 
+
+Due: 31 October 2400 h
+
+Submission: Source codes, report explaing the code and including input and output images, video file for two test sets. 
+
+
+
+  
---
+++ main.py
@@ -0,0 +1,195 @@
+
+import math
+import os
+import sys
+
+import cv2
+import numpy as np
+
+
+def test_roi(src_image, folder_name, side):
+    scale = 1
+    delta = 0
+    ddepth = cv2.CV_16S
+    THERDHOLD = 40
+
+    src = cv2.imread(os.path.join(folder_name, src_image).replace("\\", "/"))
+    src = cv2.GaussianBlur(src, (3, 3), 0)
+    gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+    grad_x = cv2.Sobel(gray, ddepth, 1, 0, ksize=3, scale=scale, delta=delta, borderType=cv2.BORDER_DEFAULT)
+    grad_y = cv2.Sobel(gray, ddepth, 0, 1, ksize=3, scale=scale, delta=delta, borderType=cv2.BORDER_DEFAULT)
+    abs_grad_x = cv2.convertScaleAbs(grad_x)
+    abs_grad_y = cv2.convertScaleAbs(grad_y)
+
+    grad = cv2.addWeighted(abs_grad_x, 0.2, abs_grad_y, 0.2, 0)
+
+    #define region of interest
+    height, width = grad.shape
+    mask = np.zeros_like(grad)
+
+    region_of_interest_vertices_right = [(0, height), (0, height-100), (250, height/2 + 50), (width, height-30), (width, height)]
+    region_of_interest_vertices_left = [(0, height), (0, height-140), (350, height/2 + 50), (width, height-30), (width, height)]
+
+    if side == "left":
+        region_of_interest = region_of_interest_vertices_left
+    else:
+        region_of_interest = region_of_interest_vertices_right    
+
+    cv2.fillPoly(mask, np.array([region_of_interest], np.int32), 255)
+
+    grad = cv2.bitwise_and(grad, mask)
+
+    #add threshold
+    #_,binary_image = cv2.threshold(grad, THERDHOLD, 255, cv2.THRESH_BINARY)
+    _,binary_image = cv2.threshold(grad, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+    #apply hough transform
+    lines = cv2.HoughLines(binary_image, rho=1, theta=np.pi/180, threshold=100)
+
+    #seperate left and right lane edges
+    lef_lines, right_lines = seperate_lines(lines)
+
+    #draw average lines and intercept
+    annotated_image = draw_average_line(lef_lines, right_lines, src)
+
+    #save image
+    folder_path = f"processed_images_{side}"
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    final_path = os.path.join(folder_path, f"{src_image}_masked.bmp".replace("\\", "/"))
+    print(final_path)
+    cv2.imwrite(final_path, annotated_image)
+    
+def seperate_lines(lines):
+    left_lines = []
+    right_lines = []
+    
+    for line in lines:
+        #calculate the slope
+        rho, theta = line[0]
+        slope = -math.cos(theta) / math.sin(theta)
+        if slope < 0:
+            left_lines.append(line)
+        else:
+            right_lines.append(line)
+    
+    return left_lines, right_lines
+
+def draw_intercept(left_x1, left_y1, left_x2, left_y2, right_x1, right_y1, right_x2, right_y2, edges):
+    # Compute the intersection point
+    left_slope = (left_y2 - left_y1) / (left_x2 - left_x1)
+    right_slope = (right_y2 - right_y1) / (right_x2 - right_x1)
+    left_intercept = left_y1 - left_slope * left_x1
+    right_intercept = right_y1 - right_slope * right_x1
+    x = int((right_intercept - left_intercept) / (left_slope - right_slope))
+    y = int(left_slope * x + left_intercept)
+
+    cv2.circle(edges, (x, y), 6, (0, 0, 255), -1)
+
+    return edges
+
+def draw_average_line(left_lines, right_lines, src_image):
+    # Variables to store the sum of rho and theta
+    sum_rho = 0
+    sum_theta = 0
+    num_lines = len(left_lines)
+
+    # Loop through each detected line
+    for line in left_lines:
+        rho, theta = line[0]  # Unpack rho and theta
+        sum_rho += rho
+        sum_theta += theta
+
+    # Compute the average rho and theta
+    avg_rho = sum_rho / num_lines
+    avg_theta = sum_theta / num_lines
+
+    # Draw the average line
+    a = np.cos(avg_theta)
+    b = np.sin(avg_theta)
+    x0 = a * avg_rho
+    y0 = b * avg_rho
+    left_x1 = int(x0 + 1000 * (-b))
+    left_y1 = int(y0 + 1000 * (a))
+    left_x2 = int(x0 - 1000 * (-b))
+    left_y2 = int(y0 - 1000 * (a))
+
+    # Create an output image
+    cv2.line(src_image, (left_x1, left_y1), (left_x2, left_y2), (255, 0, 0), 4)
+
+
+    #right lines
+    sum_rho = 0
+    sum_theta = 0
+    num_lines = len(right_lines)
+
+    # Loop through each detected line
+    for line in right_lines:
+        rho, theta = line[0]  # Unpack rho and theta
+        sum_rho += rho
+        sum_theta += theta
+
+    # Compute the average rho and theta
+    avg_rho = sum_rho / num_lines
+    avg_theta = sum_theta / num_lines
+
+    # Draw the average line
+    a = np.cos(avg_theta)
+    b = np.sin(avg_theta)
+    x0 = a * avg_rho
+    y0 = b * avg_rho
+    right_x1 = int(x0 + 1000 * (-b))
+    right_y1 = int(y0 + 1000 * (a))
+    right_x2 = int(x0 - 1000 * (-b))
+    right_y2 = int(y0 - 1000 * (a))
+
+    # Create an output image
+    cv2.line(src_image, (right_x1, right_y1), (right_x2, right_y2), (255, 0, 0), 4)
+
+    src_image = draw_intercept(left_x1, left_y1, left_x2, left_y2, right_x1, right_y1, right_x2, right_y2, src_image)
+
+    return src_image
+
+def generate_video(folder_name):
+    images = [img for img in os.listdir(folder_name) if img.endswith(".bmp")]
+    #sort images according to name
+    images.sort(key=lambda x: int(x.split("_")[1].split(".")[0]))
+    frame = cv2.imread(os.path.join(folder_name, images[0]).replace("\\", "/"))
+    height, width, layers = frame.shape
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    video = cv2.VideoWriter(f"{folder_name}.avi", fourcc, 15, (width, height))
+
+    for image in images:
+        video.write(cv2.imread(os.path.join(folder_name, image).replace("\\", "/")))
+
+    cv2.destroyAllWindows()
+    video.release()
+
+    return 0
+
+def main():
+
+    #left image processing
+    folder_name = "TestVideo_2"
+    images = [img for img in os.listdir(folder_name) if img.endswith(".bmp")]
+    for image in images:
+        print(image, folder_name)
+        test_roi(image, folder_name, "left")
+
+    #generate video
+    generate_video("processed_images_left")
+    
+    #right image processing
+    folder_name = "TestVideo_1"
+    images = [img for img in os.listdir(folder_name) if img.endswith(".bmp")]
+    for image in images:
+        print(image, folder_name)
+        test_roi(image, folder_name, "right")
+    
+    #generate video
+    generate_video("processed_images_right")
+
+    return 0
+
+if __name__ == "__main__":
+    main()
